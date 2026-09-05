6c4ebcd7040cde33dd2abdb8b570bd26f80a2076
sachin <sachncs@gmail.com>
2026-09-06 01:28:50 +0530

    M0 #16: add app/api/__init__.py
    Re-exports register() functions for each route module.

---
+++ app/api/__init__.py
@@ -0,0 +1,15 @@
+from .batch import register as register_batch
+from .health import register as register_health
+from .jobs import register as register_jobs
+from .policies import register as register_policies
+from .redact import register as register_redact
+from .stream import register as register_stream
+
+__all__ = [
+    "register_batch",
+    "register_health",
+    "register_jobs",
+    "register_policies",
+    "register_redact",
+    "register_stream",
+]
